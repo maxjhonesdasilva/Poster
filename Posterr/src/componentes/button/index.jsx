44ab762db21d3403db18fbb8cb8675bfcdc6ed30
Max Jhones <maxjhonesdasilva@gmail.com>
2024-09-04 22:46:18 -0300

    added

---
+++ Posterr/src/componentes/button/index.jsx
@@ -0,0 +1,3 @@
+export default function Button() {
+    return <button>Comprar Agora!</button>
+}
--- Posterr/src/componentes/card/index.jsx
+++ Posterr/src/componentes/card/index.jsx
@@ -1,4 +1,5 @@
 import posterImg from "../../assets/sw-poster.jpg"
+import Button from "../button"
 import styles from "./style.module.css"
 
 export default function Card() {
@@ -9,7 +10,8 @@ export default function Card() {
             <div>
                 <h2>Poster pro filme Star wars</h2>
                 <p>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
-                <button>Comprar Agora!</button>
+                <Button />
+                <Button />
             </div>
         </div>
     )
