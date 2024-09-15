362cdf2e60f4c5ca3fecb48bd45c825fe34e382b
Max Jhones <maxjhonesdasilva@gmail.com>
2024-09-14 22:41:29 -0300

    aterado a imagem

--- cartao/src/App.jsx
+++ cartao/src/App.jsx
@@ -4,10 +4,10 @@ export default function App() {
   return (
     <div className="app">
       <Profile
-        avatar=""
+        avatar="https://mail.google.com/mail/u/0?ui=2&ik=3579c09b71&attid=0.1&permmsgid=msg-a:r-1499683952209808198&th=191ee48694f3f7e0&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ8PtkGUHsA0aAKVsFslidmX-07oFes--72LZSM_EvBONhZbwmgXkF3TEtUWJlX6Y0XsDpFDinLHcOC7PXCzuzEhr0ZrbVlauR6GIAXfUp0mPnsDkegIHFqYXqY&disp=emb&realattid=ii_m11iavyn1"
         name="Max Jhones de Souza"
         bio="Full-stack javascript developer at Acme Inc."
-        email="john.doe@email.com"
+        email="maxjhonesdasilva@gmail.com"
         phone="+5527995235345"
         githubUrl="https://github.com/maxjhonesdasilva"
         linkedinUrl="https://www.linkedin.com/in/max-jhones-de-souza-silva-290ab3307/"
@@ -16,3 +16,5 @@ export default function App() {
     </div>
   )
 }
+//
+//
